530197ef9f8b2d5d148baffa58ad35b9fa60efda
Alev03YT <55165974+Alev03YT@users.noreply.github.com>
2026-02-18 19:53:07 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip);
 export default defineConfig({
   base: "/ore-stipendio/",
   plugins: [react()],
