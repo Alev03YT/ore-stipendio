801b18ca0c0aa73d136864fecb272dcddfbeef83
Alev03YT <55165974+Alev03YT@users.noreply.github.com>
2026-02-19 18:39:00 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// GitHub Pages: https://<user>.github.io/<repo>/
 export default defineConfig({
-  base: "./",
   plugins: [react()],
+  base: "/ore-stipendio/",
 });
